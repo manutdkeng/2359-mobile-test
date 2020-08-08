10745ad7a249769fc4ad2f09258fb09467443f7f
Zack Keng <zack.keng@gmail.com>
2020-08-08 13:14:14 +0800

    initial commit

---
+++ app/src/main/java/com/zack/android/test/cardgame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zack.android.test.cardgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
